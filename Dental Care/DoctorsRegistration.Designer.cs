0c09d31138433b42a542877bfc4361ab84d642b5
murshidmac <mh.murshidco.uk@outlook.com>
2019-08-23 22:52:47 +0530

    doctors settings done

--- Dental Care/DoctorsRegistration.Designer.cs
+++ Dental Care/DoctorsRegistration.Designer.cs
@@ -41,12 +41,18 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(118, 71);
+            this.label1.Location = new System.Drawing.Point(53, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
@@ -55,7 +61,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(118, 108);
+            this.label2.Location = new System.Drawing.Point(53, 215);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 1;
@@ -64,16 +70,17 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(118, 154);
+            this.label3.Location = new System.Drawing.Point(53, 91);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.Size = new System.Drawing.Size(26, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Date Of Birth";
+            this.label3.Text = "Age";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(118, 194);
+            this.label4.Location = new System.Drawing.Point(53, 136);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(135, 13);
             this.label4.TabIndex = 3;
@@ -82,7 +89,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(118, 245);
+            this.label5.Location = new System.Drawing.Point(53, 174);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(84, 13);
             this.label5.TabIndex = 4;
@@ -91,7 +98,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(118, 286);
+            this.label6.Location = new System.Drawing.Point(53, 258);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(102, 13);
             this.label6.TabIndex = 5;
@@ -99,49 +106,49 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(336, 71);
+            this.textBox1.Location = new System.Drawing.Point(211, 40);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(187, 20);
             this.textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(336, 108);
+            this.textBox2.Location = new System.Drawing.Point(211, 84);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(187, 20);
             this.textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(336, 147);
+            this.textBox3.Location = new System.Drawing.Point(211, 129);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(187, 20);
             this.textBox3.TabIndex = 8;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(336, 187);
+            this.textBox4.Location = new System.Drawing.Point(211, 171);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(187, 20);
             this.textBox4.TabIndex = 9;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(336, 238);
+            this.textBox5.Location = new System.Drawing.Point(211, 212);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(187, 20);
             this.textBox5.TabIndex = 10;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(336, 286);
+            this.textBox6.Location = new System.Drawing.Point(211, 255);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(187, 20);
             this.textBox6.TabIndex = 11;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(219, 338);
+            this.button1.Location = new System.Drawing.Point(128, 442);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(179, 51);
             this.button1.TabIndex = 12;
@@ -149,11 +156,67 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 298);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Registration Fee";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(211, 295);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(187, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(53, 347);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Username";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(53, 395);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Password";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(211, 344);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(187, 20);
+            this.textBox8.TabIndex = 18;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(211, 392);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.PasswordChar = '*';
+            this.textBox9.Size = new System.Drawing.Size(187, 20);
+            this.textBox9.TabIndex = 19;
+            this.textBox9.UseSystemPasswordChar = true;
+            // 
             // DoctorsRegistration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(628, 435);
+            this.ClientSize = new System.Drawing.Size(444, 518);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -189,5 +252,11 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
     }
 }
--- Dental Care/DoctorsRegistration.cs
+++ Dental Care/DoctorsRegistration.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Dental_Care
 {
@@ -20,34 +21,68 @@ namespace Dental_Care
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Alert the User to create the necessary details
 
             var name = textBox1.Text;
-            var address = textBox2.Text;
-            var dateofbirth = textBox3.Text;
-            var nic = textBox4.Text;
-            var contactnumber = textBox5.Text;
+            var age = textBox2.Text;
+            var nic = textBox3.Text;
+            var contactnumber = textBox4.Text;
+            var address = textBox5.Text;
             var yearsofexperience = textBox6.Text;
- 
-            string query = "INSERT INTO `dentalcare`.`doctors`(`name`,`address`,`dateofbirth`,`nic`,`contactNumber`,`yearsofexperience`)VALUES ('" + name + "','" + address + "','" + dateofbirth + "','" + nic + "','"+ contactnumber + "','" + yearsofexperience +"');";
+            var registrationFee = textBox7.Text;
+            var username = textBox8.Text;
+            var password = textBox9.Text;
+
+            string query = "INSERT INTO `dentalcare`.`doctors`(`name`,`address`,`dateofbirth`,`nic`,`contactNumber`,`yearsofexperience`)VALUES ('" + name + "','" + address + "','" + age + "','" + nic + "','"+ contactnumber + "','" + yearsofexperience +"');";
 
             Boolean output = database.Insert(query);
 
             if (output == true)
             {
-                MessageBox.Show("Doctors Details Created");
-                textBox1.Text = "";
-                textBox2.Text = "";
-                textBox3.Text = "";
-                textBox4.Text = "";
-                textBox5.Text = "";
-                textBox6.Text = "";
+                if (output == true)
+                {
+                    string sqlserverquery = "INSERT INTO [dbo].[User] ([Name],[Age],[NIC],[Contact],[Address],[YOE],[RegistrationFee],[Username],[Password],[RoleID])" +
+                 " VALUES ( @Name, @Age,@NIC, @Contact, @Address, @YOE, @RegistrationFee, @Username,@Password, @RoleID)";
+                    string connetionString = "Data Source=DESKTOP-S6B08UF\\SQLEXPRESS;Initial Catalog=DentalDB;Integrated Security=True";
+                    SqlConnection con = new SqlConnection(connetionString);
+                    SqlCommand cmd = new SqlCommand(sqlserverquery, con);
+
+                    cmd.Parameters.Add("@Name", SqlDbType.VarChar, 50).Value = name;
+                    cmd.Parameters.Add("@Age", SqlDbType.VarChar, 50).Value = age;
+                    cmd.Parameters.Add("@NIC", SqlDbType.VarChar, 50).Value = nic;
+                    cmd.Parameters.Add("@Contact", SqlDbType.VarChar, 50).Value = contactnumber;
+                    cmd.Parameters.Add("@Address", SqlDbType.VarChar, 50).Value = address;
+                    cmd.Parameters.Add("@YOE", SqlDbType.Int).Value = yearsofexperience;
+                    cmd.Parameters.Add("@RegistrationFee", SqlDbType.VarChar, 50).Value = registrationFee;
+                    cmd.Parameters.Add("@Username", SqlDbType.VarChar, 50).Value = username;
+                    cmd.Parameters.Add("@Password", SqlDbType.VarChar, 50).Value = password;
+                    cmd.Parameters.Add("@RoleID", SqlDbType.Int).Value = 2;
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    textBox3.Text = "";
+                    textBox4.Text = "";
+                    textBox5.Text = "";
+                    textBox6.Text = "";
+                    textBox7.Text = "";
+                    textBox8.Text = "";
+                    textBox9.Text = "";
+                    MessageBox.Show("Doctors Details Created");
+
+                }
             }
             else
             {
                 MessageBox.Show("Doctors Details not created");
             }
 
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
 
         }
     }
