49f215e9bd2250f3b6e0e2a6a11c5f78ed9be5cc
murshidmac <mh.murshidco.uk@outlook.com>
2019-08-24 00:36:48 +0530

    appointment done almost need to fix the date time issue

--- Dental Care/AppointPatientDetails.cs
+++ Dental Care/AppointPatientDetails.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace Dental_Care
 {
     public partial class AppointPatientDetails : Form
@@ -22,6 +22,30 @@ namespace Dental_Care
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var date = textBox1.Text;
+            var amount = textBox2.Text;
+            MessageBox.Show(date.Trim());
+            DateTime oDate = Convert.ToDateTime(date.Trim());
+
+            MessageBox.Show(date);
+
+            string query = "INSERT INTO [dbo].[Appointment] ([DocName] ,[Date],[treatment_ID]) VALUES (@DocName, @AppointmentDate , (SELECT TOP 1 [Code] FROM Treatment where Amount= @Appointmentamount))";
+            string connetionString = "Data Source=DESKTOP-S6B08UF\\SQLEXPRESS;Initial Catalog=DentalDB;Integrated Security=True";
+            SqlConnection con = new SqlConnection(connetionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.Add("@DocName", SqlDbType.VarChar, 50).Value = this.doctorSelected;
+            cmd.Parameters.Add("@AppointmentDate", SqlDbType.Date).Value = oDate;
+            cmd.Parameters.Add("@Appointmentamount", SqlDbType.Int).Value = amount;
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Appointment Made");
         }
     }
 }
--- Dental Care/Appointments.cs
+++ Dental Care/Appointments.cs
@@ -7,19 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Dental_Care
 {
     public partial class Appointments : Form
     {
+        List<string> listofDoctors;
+
         public Appointments()
         {
             InitializeComponent();
+            this.getDatabaseList();
         }
+
+        List<string> getDatabaseList()
+        {
+            string connetionString = "Data Source=DESKTOP-S6B08UF\\SQLEXPRESS;Initial Catalog=DentalDB;Integrated Security=True";
+
+            SqlConnection con = new SqlConnection(connetionString);
+
+            con.Open();
+
+            string query = "SELECT [Name] FROM [dbo].[User] WHERE RoleID=2;";
+            SqlCommand command = new SqlCommand(query, con);
+
+            IDataReader dr = command.ExecuteReader();
+            listofDoctors = new List<string>();
+
+                while (dr.Read())
+                {
+                    listofDoctors.Add(dr[0].ToString());
+                    MessageBox.Show(dr[0].ToString());
+                }
+
+            listofDoctors.ForEach(elment =>
+            {
+                comboBox1.Items.Add(elment);
+            });
+            return listofDoctors;
+        }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
             string doctorsSelection = comboBox1.Text;
+
             AppointPatientDetails form = new AppointPatientDetails(doctorsSelection);
             form.Show();
         }
